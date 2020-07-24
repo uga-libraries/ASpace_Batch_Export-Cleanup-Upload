c213a744029a5b4ff5a8b139b722b8e6d8d38566
schmidtster <cschmidtuga@gmail.com>
2020-07-24 14:37:22 -0400

    Update version #

--- as_xtf_GUI.py
+++ as_xtf_GUI.py
@@ -363,7 +363,7 @@ def run_gui(defaults):
             # TODO Change Version #
             layout_about = [
                 [sg.Text("Created by Corey Schmidt for the University of Georgia Libraries\n\n"
-                         "Version: 0.6.2\n\n"
+                         "Version: 1.0.0\n\n"
                          "To check for the latest versions, check the Github\n", font=("Roboto", 12))],
                 [sg.OK(bind_return_key=True, key="_ABOUT_OK_"), sg.Button(" Check Github ", key="_CHECK_GITHUB_")]
             ]
